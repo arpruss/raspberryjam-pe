6362e981d73ea85b724c8ab7a218d3adb6e0c77f
arpruss <arpruss@gmail.com>
2015-06-04 09:16:08 -0500

    foo

--- p2/mc.py
+++
@@ -1,8 +0,0 @@
-#
-# Public domain code by Alexander Pruss
-#
-
-from mcpi.minecraft import *
-from mcpi.entity import *
-from mcpi.block import *
-from math import *
--- p2/turtle.py
+++
@@ -1,6 +0,0 @@
-#
-# Public domain code by Alexander Pruss
-#
-# for backwards compatibility with earlier versions of
-# Pruss's instructable
-from mcturtle import *
